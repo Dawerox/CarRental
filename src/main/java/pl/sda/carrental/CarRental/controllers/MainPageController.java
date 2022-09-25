df2b16772e2826a939920473f69a5060c6fbc163
AdamGawryszek <adam.gawryszek@gmail.com>
2022-09-25 12:58:49 +0200

    html

--- src/main/java/pl/sda/carrental/CarRental/controllers/MainPageController.java
+++ src/main/java/pl/sda/carrental/CarRental/controllers/MainPageController.java
@@ -12,8 +12,44 @@ public class MainPageController {
         return "index";
     }
 
-    @GetMapping("/about.html")
+    @GetMapping("/about")
     public String getAboutPage(Model model) {
         return "about";
     }
+
+    @GetMapping("/agent-single")
+    public String getagentsinglePage(Model model) {
+        return "agent-single";
+    }
+
+    @GetMapping("/agents-grid")
+    public String getagentsgirdPage(Model model) {
+        return "agents-grid";
+    }
+
+    @GetMapping("/blog-grid")
+    public String getbloggrid(Model model) {
+        return "blog-grid";
+    }
+
+    @GetMapping("/contact")
+    public String getcontactPage(Model model) {
+        return "contact";
+    }
+
+    @GetMapping("/property-grid")
+    public String getpropertygridPage(Model model) {
+        return "property-grid";
+    }
+
+    @GetMapping("/property-single")
+    public String getpropertysinglePage(Model model) {
+        return "property-single";
+    }
+
+    @GetMapping("/blog-single")
+    public String getblogsinglePage(Model model) {
+        return "blog-single";
+    }
+
 }
