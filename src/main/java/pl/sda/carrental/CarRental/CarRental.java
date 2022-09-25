fdd2398373c0c6e479a75038893b7b9d676a481a
Dawer <dawerox@gmail.com>
2022-09-25 15:01:13 +0200

    fragments 1 menu

--- src/main/java/pl/sda/carrental/CarRental/CarRental.java
+++ src/main/java/pl/sda/carrental/CarRental/CarRental.java
@@ -9,6 +9,8 @@ public class CarRental {
     public void addCar(Car car){
         Cars.put(car.getCarId(), car);
     }
+    Car car = new Car("AudiA3","A3",2016,"Audi","Hatchback");
+    Car car2 = new Car("BMWm3","m3",2015,"BMW","sedan");
 
 
 
